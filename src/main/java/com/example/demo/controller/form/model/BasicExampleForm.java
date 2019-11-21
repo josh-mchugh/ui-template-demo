11231c21b35f1e843027d20f8ec9fffbcd37855a
jmchugh <joshua.mchugh01@gmail.com>
2019-11-20 22:59:29 -0500

    Added component for select element

--- src/main/java/com/example/demo/controller/form/model/BasicExampleForm.java
+++ src/main/java/com/example/demo/controller/form/model/BasicExampleForm.java
@@ -1,12 +1,34 @@
 package com.example.demo.controller.form.model;
 
+import com.google.common.collect.Lists;
 import lombok.Data;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Data
 public class BasicExampleForm {
+
 
     private String email;
     private String password;
     private String textArea;
     private boolean terms;
+
+    private Map<String, String> countries;
+    private String selectedCountry;
+    private List<String> selectedCountries;
+
+    public BasicExampleForm() {
+
+        this.countries = Stream.of(Locale.getISOCountries())
+                .map(code -> new Locale("", code))
+                .collect(Collectors.toMap(Locale::getCountry, Locale::getDisplayCountry));
+
+        this.selectedCountry = "US";
+        this.selectedCountries = Lists.newArrayList("US", "GB", "CA");
+    }
 }
